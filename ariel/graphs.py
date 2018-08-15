7deb07b7188f45759daaefdc1762c4979c13a9a1
Eric Jenny <erc.hakawa@gmail.com>
2018-08-15 12:08:38 -0400

    Generated more graphs

--- ariel/graphs.py
+++ ariel/graphs.py
@@ -12,41 +12,42 @@ if __name__ == "__main__":
     filename = "data/2018-07-19_09.42/"
     filename = "data/2018-07-27_16.40/"
     filename = "data/2018-08-02_16.32/"
-    dfx = pd.read_csv(filename + "xNew.csv", header=None)
+    filename = "data/2018-08-10_14.45/"
+    dfx = pd.read_csv(filename + "x.csv", header=None)
     # print dfx
 
-    # for i in range (0, 10):
-    #     x = dfx[i]
-    #     count, bins = np.histogram(x, bins=50, density=True)
-    #     plt.plot(bins[:-1], count)
-    # plt.xlim([0,1])
-    # title = "Histogram of Firefly Algorithm Generated Antenna Arrays"
-    # plt.title(title)
-    # plt.savefig("graphs/" + title)
+    for i in range (0, 10):
+        x = dfx[i]
+        count, bins = np.histogram(x, bins=50, density=True)
+        plt.plot(bins[:-1], count)
+    plt.xlim([0,1])
+    title = "Histogram of Firefly Algorithm Generated Antenna Arrays"
+    plt.title(title)
+    plt.savefig("graphs/" + title)
+    plt.show()
+
+    # plotting.scatter_matrix(dfx, alpha=0.2, figsize=(6, 6))
     # plt.show()
 
-    # # plotting.scatter_matrix(dfx, alpha=0.2, figsize=(6, 6))
-    # # plt.show()
+    count, bins = np.histogram(dfx, bins=128, range=[0, 1], density=True)
+    cdf = np.cumsum(count)/128
+    plt.plot(bins[:-1],cdf)
+    plt.show()
 
-    # count, bins = np.histogram(dfx, bins=128, range=[0, 1], density=True)
-    # cdf = np.cumsum(count)/128
-    # plt.plot(bins[:-1],cdf)
-    # plt.show()
+    dfx.hist(color='aqua', alpha=0.5, bins=50)
+    plt.title("Positions Histogram")
+    plt.xlim(0.0,1.0)
+    plt.show()
 
-    # dfx.hist(color='aqua', alpha=0.5, bins=50)
-    # plt.title("Positions Histogram")
-    # plt.xlim(0.0,1.0)
-    # plt.show()
+    print dfx.quantile([0.25, 0.5, 0.75])
+    dfx.boxplot()
+    plt.show()
 
-    # print dfx.quantile([0.25, 0.5, 0.75])
-    # dfx.boxplot()
-    # plt.show()
-
-    # cor = dfx.corr()
-    # print cor
-    # plt.matshow(cor)
-    # plt.colorbar()
-    # plt.show()
+    cor = dfx.corr()
+    print cor
+    plt.matshow(cor)
+    plt.colorbar()
+    plt.show()
 
     dif = dfx.diff(axis=1)
     dif[10] = 1 - dfx[9]
@@ -76,19 +77,19 @@ if __name__ == "__main__":
 
 
     num = len(dif.columns)
-    # # print num
-    # fig, axs = plt.subplots(ncols=num, nrows=num, sharey=True, sharex=True, figsize=(6, 6))
-    # # fig.subplots_adjust(hspace=0.5, left=0.07, right=0.93)
-    # for i in range (0, num):
-    #     for j in range (0, num):
-    #         ax = axs[i][j]
-    #         hb = ax.hexbin(dif.iloc[:,i], dif.iloc[:,j], gridsize=20, cmap='inferno', extent=[0.0, 0.3, 0.0, 0.3])
-    #         ax.axis([0.0, 0.3, 0.0, 0.3])
-    #         ax.axis('off')
-    #         # ax.set_title("Hexagon binning")
-    #         # cb = fig.colorbar(hb, ax=ax)
-    #         # cb.set_label('counts')
-    # plt.show()
+    # print num
+    fig, axs = plt.subplots(ncols=num, nrows=num, sharey=True, sharex=True, figsize=(6, 6))
+    # fig.subplots_adjust(hspace=0.5, left=0.07, right=0.93)
+    for i in range (0, num):
+        for j in range (0, num):
+            ax = axs[i][j]
+            hb = ax.hexbin(dif.iloc[:,i], dif.iloc[:,j], gridsize=20, cmap='inferno', extent=[0.0, 0.3, 0.0, 0.3])
+            ax.axis([0.0, 0.3, 0.0, 0.3])
+            ax.axis('off')
+            # ax.set_title("Hexagon binning")
+            # cb = fig.colorbar(hb, ax=ax)
+            # cb.set_label('counts')
+    plt.show()
 
 
     num = len(dif.columns)
